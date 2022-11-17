826c092804469bcc7efc7c5bbf3991ecef4169d9
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-17 13:31:43 +0200

    sass path

---
+++ dist/index.c137cb32.js
@@ -0,0 +1,2 @@
+!function(){function t(){this.classList.toggle("rotated")}document.querySelectorAll(".card").forEach((c=>c.addEventListener("click",t)))}();
+//# sourceMappingURL=index.c137cb32.js.map
