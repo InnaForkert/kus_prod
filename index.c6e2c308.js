73e9362ec05889b6057967f2f1466dfae53f3297
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-17 11:33:50 +0000

    Deploying to gh-pages from @ InnaForkert/kus_prod@978ce1803c8e8b5754dcf42724475e692b782ae1 🚀

--- index.c6e2c308.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll(".card");
-cards.forEach((card)=>card.addEventListener("click", handleCardClick));
-function handleCardClick() {
-    this.classList.toggle("rotated");
-}
-
-//# sourceMappingURL=index.c6e2c308.js.map
