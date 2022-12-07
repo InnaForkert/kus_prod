4243433fc15f529e7857de950eacbb76ddb10f0c
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-08 01:22:04 +0200

    mobile menu

--- dist/index.30cea8d5.js
+++ dist/index.30cea8d5.js
@@ -532,12 +532,31 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"1OFUP":[function(require,module,exports) {
-const closeMobileMenuBtn = document.querySelector(".mobile-menu-close");
 const mobileMenu = document.querySelector(".mobile-menu");
-console.log("hi");
-closeMobileMenuBtn.addEventListener("click", closeMobileMenu);
+const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+const circles = document.querySelectorAll(".mobile-menu-btn svg");
+const mobileMenuLinks = document.querySelectorAll(".mobile-menu-link");
+mobileMenuBtn.addEventListener("click", openMobileMenu);
+mobileMenuLinks.forEach((link)=>{
+    link.addEventListener("click", closeMobileMenu);
+});
+function openMobileMenu() {
+    mobileMenu.classList.remove("mobile-menu-onClose");
+    circles[1].classList.add("move-top");
+    circles[3].classList.add("move-left");
+    circles[5].classList.add("move-right");
+    circles[7].classList.add("move-bottom");
+    mobileMenuBtn.removeEventListener("click", openMobileMenu);
+    mobileMenuBtn.addEventListener("click", closeMobileMenu);
+}
 function closeMobileMenu() {
     mobileMenu.classList.add("mobile-menu-onClose");
+    circles[1].classList.remove("move-top");
+    circles[3].classList.remove("move-left");
+    circles[5].classList.remove("move-right");
+    circles[7].classList.remove("move-bottom");
+    mobileMenuBtn.addEventListener("click", openMobileMenu);
+    mobileMenuBtn.removeEventListener("click", closeMobileMenu);
 }
 
 },{}]},["5JFHQ","1OFUP"], "1OFUP", "parcelRequire8436")
--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -1,10 +1,29 @@
-const closeMobileMenuBtn = document.querySelector(".mobile-menu-close");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+const circles = document.querySelectorAll(".mobile-menu-btn svg");
+const mobileMenuLinks = document.querySelectorAll(".mobile-menu-link");
 
-console.log("hi");
+mobileMenuBtn.addEventListener("click", openMobileMenu);
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", closeMobileMenu);
+});
 
-closeMobileMenuBtn.addEventListener("click", closeMobileMenu);
+function openMobileMenu() {
+  mobileMenu.classList.remove("mobile-menu-onClose");
+  circles[1].classList.add("move-top");
+  circles[3].classList.add("move-left");
+  circles[5].classList.add("move-right");
+  circles[7].classList.add("move-bottom");
+  mobileMenuBtn.removeEventListener("click", openMobileMenu);
+  mobileMenuBtn.addEventListener("click", closeMobileMenu);
+}
 
 function closeMobileMenu() {
   mobileMenu.classList.add("mobile-menu-onClose");
+  circles[1].classList.remove("move-top");
+  circles[3].classList.remove("move-left");
+  circles[5].classList.remove("move-right");
+  circles[7].classList.remove("move-bottom");
+  mobileMenuBtn.addEventListener("click", openMobileMenu);
+  mobileMenuBtn.removeEventListener("click", closeMobileMenu);
 }
