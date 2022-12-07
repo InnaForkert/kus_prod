16e2d6254f95d0c21ac17dcb15e77afb5dd2d5a9
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-07 23:36:39 +0000

    Deploying to gh-pages from @ InnaForkert/kus_prod@f6b887eca660567256351301d8509b5ece819cbb 🚀

--- index.21cd7299.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".mobile-menu-close"),o=document.querySelector(".mobile-menu");console.log("hi"),e.addEventListener("click",(function(){o.classList.add("mobile-menu-onClose")}))}();
-//# sourceMappingURL=index.21cd7299.js.map
--- index.3aa4f57c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".mobile-menu-close"),o=document.querySelector(".mobile-menu");console.log("hi"),e.addEventListener("click",(function(){o.classList.add("mobile-menu-onClose")}));
-//# sourceMappingURL=index.3aa4f57c.js.map
