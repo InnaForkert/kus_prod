459b2e78c80042963dd6932df6bbfcce81474155
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-09 15:49:20 +0200

    aos

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import "./scripts/mobileMenu";
 import "./scripts/videos";
 import "./scripts/showFilter";
 import "./scripts/topTop";
+import "./scripts/aos";
---
+++ src/scripts/aos.js
@@ -0,0 +1,3 @@
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+AOS.init();
