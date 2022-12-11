010c9c620ed7d2b465679baeae7506c25a7bcf02
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-11 22:25:54 +0200

    removed clg

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -579,7 +579,6 @@ const cards = document.querySelectorAll(".card");
 cards.forEach((card)=>card.addEventListener("click", handleCardClick));
 cards.forEach((card)=>card.addEventListener("touchend", handleCardClick));
 function handleCardClick(e) {
-    console.log(e);
     this.classList.toggle("rotated");
 }
 
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,6 +4,5 @@ cards.forEach((card) => card.addEventListener("click", handleCardClick));
 cards.forEach((card) => card.addEventListener("touchend", handleCardClick));
 
 function handleCardClick(e) {
-  console.log(e);
   this.classList.toggle("rotated");
 }
